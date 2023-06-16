abc8ade47dee6ae61486f45d8bdd87250f00b048
bigodrigo <roesbo1@hotmail.com>
2023-06-16 11:54:00 -0300

    Layout Hero funcional

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -4,28 +4,23 @@ import profilePic from '../assets/profile.jpg'
 
 const Hero = ({heading, message}) => {
     return (
-        <div className="flex items-center justify-center h-screen bg-fixed bg-center bg-cover custom-img">
-          {/* {Overlay} */}
-          <div className="flex justify-between content-between max-w-screen-lg mx-auto">
-            <div className="flex items-center">
-              <div className="h-64 w-64 rounded-full overflow-hidden">
-                <div className="rounded-full">
-                  <Image
-                    src={profilePic}
-                    alt=""
-                    width={300}
-                    height={300}
-                  />
-                </div>
+      <div className="flex items-center justify-center h-screen bg-fixed bg-center bg-cover custom-img">
+        {/* {Overlay} */}
+        <div className="flex flex-col sm:flex-row items-center sm:justify-between content-between max-w-screen-lg mx-auto">
+          <div className="flex items-center">
+            <div className="h-64 w-64 sm:w-96 sm:h-96 rounded-full overflow-hidden">
+              <div className="rounded-full">
+                <Image src={profilePic} alt="" className="object-cover w-full h-full" />
               </div>
             </div>
-            <div className="p-5 text-palet-two-qua ml-10">
-              <h2 className="text-5xl font-bold">{heading}</h2>
-              <p className="py-5 text-xl">{message}</p>
-              <button className="px-8 py-2 border">About Me</button>
-            </div>
+          </div>
+          <div className="p-5 text-palet-two-qua mt-5 sm:mt-0 sm:ml-10">
+            <h2 className="text-5xl font-bold">{heading}</h2>
+            <p className="py-5 text-xl">{message}</p>
+            <button className="px-8 py-2 border">About Me</button>
           </div>
         </div>
+      </div>
       )
     }
 
