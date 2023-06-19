c314025b46efb3557941ab3438930ec68944c1a6
bigodrigo <roesbo1@hotmail.com>
2023-06-19 16:03:37 -0300

    ordem Web

--- src/app/components/WebData.js
+++ src/app/components/WebData.js
@@ -1,19 +1,19 @@
 export const WebData = [
-    {
-        image: require('../assets/home.png'),
-        // title: 'Saúde Web Notifications',
-        title: 'Saúde Web',
-        // description: 'This project is a website that facilitates the sending of customized messages to a mobile phone that has the Saúde App installed.',
-        description: 'Este site facilita o envio de mensagens personalizadas para um celular que tenha a App Saúde instalado. Recomendo fortemente observar o App Saúde junto deste projeto!',
-        git: 'https://github.com/Bigodrigo/SaudeWeb',
-        deploy: 'https://saude-web.vercel.app/',
-      },
-      {
-        image: require('../assets/card.png'),
-        title: 'RocketPay',
-        // description: "Rocketpay is a software feature that imitates the credit card data entry form, providing a practical way to include input mask and dynamically update HTML elements through the Document Object Model (DOM).",
-        description: "O Rocketpay é um componente que simula o formulário de preenchimento de cartão de crédito, onde é possível adicionar máscara aos inputs e atualizar elementos HTML via DOM.",
-        git: 'https://github.com/Bigodrigo/explorer-lab-01',
-        deploy: 'https://explorer-lab-01-opal-two.vercel.app/',
-      },
+  {
+    image: require('../assets/card.png'),
+    title: 'RocketPay',
+    // description: "Rocketpay is a software feature that imitates the credit card data entry form, providing a practical way to include input mask and dynamically update HTML elements through the Document Object Model (DOM).",
+    description: "O Rocketpay é um componente que simula o formulário de preenchimento de cartão de crédito, onde é possível adicionar máscara aos inputs e atualizar elementos HTML via DOM.",
+    git: 'https://github.com/Bigodrigo/explorer-lab-01',
+    deploy: 'https://explorer-lab-01-opal-two.vercel.app/',
+  },
+  {
+    image: require('../assets/home.png'),
+    // title: 'Saúde Web Notifications',
+    title: 'Saúde Web',
+    // description: 'This project is a website that facilitates the sending of customized messages to a mobile phone that has the Saúde App installed.',
+    description: 'Este site facilita o envio de mensagens personalizadas para um celular que tenha a App Saúde instalado. Recomendo fortemente observar o App Saúde junto deste',
+    git: 'https://github.com/Bigodrigo/SaudeWeb',
+    deploy: 'https://saude-web.vercel.app/',
+  },
 ];
