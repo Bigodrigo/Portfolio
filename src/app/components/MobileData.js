ed42e164f9f68951d9ce7265d3bfce91a9ee3aef
bigodrigo <roesbo1@hotmail.com>
2023-06-30 12:19:53 -0300

    ajuste urls

--- src/app/components/MobileData.js
+++ src/app/components/MobileData.js
@@ -5,7 +5,7 @@ export const MobileData = [
     title: 'Firebase Tasks App',
     // description: "This Android application is designed to effectively manage an individual's to-do tasks. The account registration system and login functionality are supported by the Firebase platform.",
     description: "Este aplicativo Android foi projetado para gerenciar as tarefas pendentes de um usuário. O sistema de registro de conta e a login são administradas pela plataforma Firebase.",
-    git: 'https://github.com/Bigodrigo/TasksFirebase',
+    git: 'https://github.com/bigodrigo/TasksFirebase',
   },
   {
     video: '/assets/SaudeApp.mp4',
@@ -13,7 +13,7 @@ export const MobileData = [
     title: 'Saúde App',
     // description: 'This project is an application designed for health plans. It specifically facilitates the transmission of information through the utilization of a virtual card via NFC and permits the reception of personalized notifications.',
     description: 'Este projeto é um aplicativo genérico para planos de saúde. Especificamente facilita a transmissão de informação através da utilização de um cartão virtual via NFC e permite a recepção de notificações (push) personalizadas.',
-    git: 'https://github.com/Bigodrigo/SaudeApp',
+    git: 'https://github.com/bigodrigo/SaudeApp',
   },
 ];
   
--- src/app/components/WebData.js
+++ src/app/components/WebData.js
@@ -5,7 +5,7 @@ export const WebData = [
     title: 'RocketPay',
     // description: "Rocketpay is a software feature that imitates the credit card data entry form, providing a practical way to include input mask and dynamically update HTML elements through the Document Object Model (DOM).",
     description: "O Rocketpay é um componente que simula o formulário de preenchimento de cartão de crédito, onde é possível adicionar máscara aos inputs e atualizar elementos HTML via DOM.",
-    git: 'https://github.com/Bigodrigo/explorer-lab-01',
+    git: 'https://github.com/bigodrigo/explorer-lab-01',
     deploy: 'https://explorer-lab-01-opal-two.vercel.app/',
   },
   {
@@ -15,7 +15,7 @@ export const WebData = [
     title: 'Saúde Web',
     // description: 'This project is a website that facilitates the sending of customized messages to a mobile phone that has the Saúde App installed.',
     description: 'Este site facilita o envio de mensagens personalizadas para um celular que tenha a App Saúde instalado. Recomendo fortemente observar o App Saúde junto deste',
-    git: 'https://github.com/Bigodrigo/SaudeWeb',
+    git: 'https://github.com/bigodrigo/SaudeWeb',
     deploy: 'https://saude-web.vercel.app/',
   },
 ];
